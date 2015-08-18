a31642133d4db264a6f682be699def959ef2f839
Alban Mulaki <amulaki@pragmaticlinux.org>
2015-08-19 01:06:30 +0200

    first

---
+++ setup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#
+# Assumptions:
+#  1) User has partitioned, formatted, and mounted partitions on /mnt
+#  2) Network is functional
+#  3) Arguments passed to the script are valid pacman targets
+#  4) A valid mirror appears in /etc/pacman.d/mirrorlist
+#
+
+#SELECT KEYMAP
+select_keymap(){
+
+}
+
+#DEFAULT EDITOR 
+select_editor(){
+
+}
+
+#MIRRORLIST
+configure_mirrorlist(){
+
+}
+
+#SELECT DEVICE 
+select_device(){
+
+}
+
+create_partition_scheme(){
+
+}
+
+#Setup pratition
+
+create_partition(){
+
+}
+
+#Format partitions
+
+format_partitions(){
+
+}
+
+
+#INSTALL BASE SYSTEM 
+install_base_system(){
+
+}
+
+#CONFIGURE KEYMAP
+configure_keymap(){
+
+}
+
+#CONFIGURE FSTAB
+configure_fstab(){
+
+}
+
+#CONFIGURE HOSTNAME
+configure_hostname(){
+
+}
+
+#CONFIGURE TIMEZONE
+configure_timezone(){
+
+}
+
+
+#CONFIGURE MKINITCPIO
+
+configure_mkinitcpio(){
+
+}
+
+
+#INSTALL BOOTLOADER 
+install_bootloader(){
+
+}
+
+configure_bootloader(){
+
+}
+
+#ROOT PASSWORD
+root_password(){
+
+}
+
+#FINISH {{{
+finish(){
+
+}
+
