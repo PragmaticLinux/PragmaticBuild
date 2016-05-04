ca6cff5ad026fa1e9e09498cb00644d5ac014076
Alban Mulaki <alban.mulaki@gmail.com>
2016-05-04 17:49:22 +0200

    Chroot system,  user configure, configure file system and devices, configure bootloader

--- praginstall.sh
+++ praginstall.sh
@@ -20,8 +20,61 @@ echo "|_|   |_|  \__,_|\__, |_| |_| |_|\__,_|\__|_|\___| |______|_|_| |_|\__,_/_
 echo "                  __/ |                                                       "
 echo "                 |___/                                                        "
 
+
+PRAGMATIC_SYSTEM_MOUNT="/mnt"
+PRAGMATIC_SYSTEM_PARTITION=""
+SWAP_PARTITION=""
+
+
+
 install_pragmaticLinux(){
+	filesystem_setup
+	cd $PRAGMATIC_SYSTEM_MOUNT
+	tar -xf /PragmaticLinux*
+	rm $PRAGMATIC_SYSTEM_MOUNT/etc/fstab
+	genfstab /mnt >> /mnt/etc/fstab
+	chroot_system
+	configure_users
+	setup_bootloader
+	
 	echo "Installation begins"
+}
+
+configure_system(){
+	chroot ./ /bin/bash -c "mkinitcpio -p linux"
+	
+}
+
+
+
+# DEVICE FIX  postfix BUG
+setup_bootloader(){
+	chroot ./ /bin/bash -c "grub-install $PRAGMATIC_SYSTEM_MOUNT"
+	chroot ./ /bin/bash -c "grub-mkconfig -o /boot/grub/grub"
+	
+}
+
+configure_users(){
+	chroot ./ /bin/bash -c "passwd"
+	echo "Create your username"
+	read $username
+	chroot ./ /bin/bash -c "useradd -m -g users -G wheel,storage,power -s /bin/bash  $username"
+	
+}
+
+chroot_system(){
+	mount -t proc proc proc/
+	mount --rbind /sys sys/
+	mount --rbind /dev dev/
+	mount --rbind /run run/
+	chroot ./
+}
+
+filesystem_setup(){
+	mkfs.ext4 $PRAGMATIC_SYSTEM_PARTITION
+	mkswap $SWAP_PARTITION
+	mount $PRAGMATIC_SYSTEM_PARTITION
+	swapon $SWAP_PARTITION
 }
 
 
@@ -49,10 +102,12 @@ while [ "$confirm_partition" = false ]; do
 	read -p "Do you wish to install Pragmatic Linux as you see partition (Yy,Nn)?" yn
     	case $yn in
      		[Yy]* ) confirm_partition=""
-			install_pragmaticLinux break;;
+			PRAGMATIC_SYSTEM_PARTITION=PragmaticSystemPartition
+			SWAP_PARTITION=SwapPartition
+			install_pragmaticLinux
+			break;;
         	[Nn]* ) set_partition;;
         	* ) echo "Please answer yes or no.";;
     	esac
 done
 
-#arch-chroot ./work/airootfs /bin/bash -c uname -a
